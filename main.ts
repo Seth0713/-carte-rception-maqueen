3892bd444eb0844e60f507e5ad4a59ce42e5e222
Seth0713 <jb.giuliana@protonmail.com>
2022-06-26 18:14:42 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,15 +5,21 @@ let Recep = 0
 radio.setGroup(1)
 basic.forever(function () {
     if (Recep == 1) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 100)
-        basic.showNumber(Recep)
-    }
-    if (Recep == 2) {
+        maqueen.motorStop(maqueen.Motors.All)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 100)
         basic.showNumber(Recep)
     }
+    if (Recep == 2) {
+        maqueen.motorStop(maqueen.Motors.All)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 100)
+        basic.showNumber(Recep)
+    }
     if (Recep == 3) {
+        maqueen.motorStop(maqueen.Motors.All)
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
         basic.showNumber(Recep)
+    }
+    if (Recep == 4) {
+        maqueen.motorStop(maqueen.Motors.All)
     }
 })
