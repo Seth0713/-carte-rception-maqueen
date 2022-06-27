dd60f38cfd1dd3abeaba3de616fd3327c166a565
Seth0713 <jb.giuliana@protonmail.com>
2022-06-27 08:56:43 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,19 +7,47 @@ basic.forever(function () {
     if (Recep == 1) {
         maqueen.motorStop(maqueen.Motors.All)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 100)
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
         basic.showNumber(Recep)
     }
     if (Recep == 2) {
         maqueen.motorStop(maqueen.Motors.All)
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 100)
         basic.showNumber(Recep)
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
     }
     if (Recep == 3) {
         maqueen.motorStop(maqueen.Motors.All)
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
         basic.showNumber(Recep)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
     }
     if (Recep == 4) {
         maqueen.motorStop(maqueen.Motors.All)
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
     }
 })
